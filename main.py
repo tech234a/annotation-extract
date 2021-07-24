7b7402b9470849b8b3bc9e1dfbbddb6a84c8cc03
tech234a <46801700+tech234a@users.noreply.github.com>
2021-07-24 18:54:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ urlsl = set()
 
 # https://stackoverflow.com/a/19587581
 for file in tar:
-    if 1:
+    if file:
         urls = ET.parse(tar.extractfile(file).read()).getroot().findall('.//url')
         for tag in urls:
             urlint = tag.attrib['value']
