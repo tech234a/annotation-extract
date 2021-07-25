9a9872b1425e96ac87ce2ba9bdb87f61d8bf0945
tech234a <46801700+tech234a@users.noreply.github.com>
2021-07-24 22:13:50 -0400

    Various fixes

--- main.py
+++ main.py
@@ -2,17 +2,20 @@
 
 print("Initializing...")
 
-from os import system, environ, walk
-from os.path import join
-import heroku3
-import tarfile
+from os import system, environ
+import heroku3, tarfile
 from urllib.request import urlretrieve
 import xml.etree.ElementTree as ET
+from time import sleep
+
+#From https://github.com/iv-org/invidious/blob/ea0d52c0b85c0207c1766e1dc5d1bd0778485cad/src/invidious.cr#L79
+CHARS_SAFE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"
 
 system("git config --global user.name \"annotation-extract-bot\"")
 system("git config --global user.email annotation-extract-bot@annotation-extract-bot.local")
 
 url = environ["url"] # ex https://archive.org/download/youtubeannotations_00/A-.tar
+print(url)
 item = environ["url"].split("/")[-1].split(".")[0]
 
 print("Downloading tar file (this may take a while)...")
@@ -26,14 +29,16 @@ urlsl = set()
 
 # https://stackoverflow.com/a/19587581
 for file in tar:
-    if file and tar.extractfile(file):
-        urls = ET.fromstring(tar.extractfile(file).read()).getroot().findall('.//url')
-        for tag in urls:
-            urlint = tag.attrib['value']
-            if urlint.startswith("https://www.youtube.com/watch?"):
-                vidsl.add(urlint.split("v=")[-1].split("&")[0])
-            else:
-                urlsl.add(urlint.removeprefix("https://www.youtube.com/"))
+    if file:
+        myfi = tar.extractfile(file)
+        if myfi:
+            urls = ET.parse(myfi).getroot().findall('.//url')
+            for tag in urls:
+                urlint = tag.attrib['value']
+                if urlint.startswith("https://www.youtube.com/watch?"):
+                    vidsl.add(urlint.split("v=")[-1].split("&")[0])
+                else:
+                    urlsl.add(urlint.removeprefix("https://www.youtube.com/"))
 
 system("git clone "+environ["git-url"]+" repo")
 urlf = open("repo/"+item+"_urls.txt", "w")
@@ -47,4 +52,29 @@ for item in vidsl:
 vidf.close()
 
 system("cd repo; git add .; git commit -m \"Add "+item+"\"; git push")
-heroku3.from_key(environ['heroku-key']).apps()[environ['heroku-app']].scale_formation_process('worker', 0)
+
+applist = heroku3.from_key(environ['heroku-key']).apps(order_by="name")
+currentlists = []
+for app in applist:
+    if app.name.startswith("annotation"):
+        currentlists.append(int(app.config()["url"].split("/")[-1].split(".")[0]))
+        
+desl = "A"
+for item in currentlists:
+    if CHARS_SAFE.index(item[0]) > CHARS_SAFE.index(desl):
+        desl = item[0]
+
+deslb = "A"
+for item in currentlists:
+    if CHARS_SAFE.index(item[1]) > CHARS_SAFE.index(desl):
+        desl = CHARS_SAFE[CHARS_SAFE.index(item[1]) + 1]
+
+if CHARS_SAFE.index(deslb) > 63:
+    desl = CHARS_SAFE[CHARS_SAFE.index(desl) + 1]
+
+if CHARS_SAFE.index(desl) >= 63 and CHARS_SAFE.index(deslb) > 63:
+    heroku3.from_key(environ['heroku-key']).apps()[environ['heroku-app']].scale_formation_process('worker', 0)
+else:
+    heroku3.from_key(environ['heroku-key']).apps()[environ['heroku-app']].config()["url"] = "https://archive.org/download/youtubeannotations_"+str(CHARS_SAFE.index(desl)).zfill(2)+"/"+desl+deslb+".tar"
+
+sleep(1)
